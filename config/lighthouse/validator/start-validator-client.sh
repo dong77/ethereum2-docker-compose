bb25c732504f0518a10f1b481d6e533e6586afd2
wangdong <wangdong@zhongan.io>
2020-12-03 10:07:34 +0800

    disable creating new keys

--- config/lighthouse/validator/start-validator-client.sh
+++ config/lighthouse/validator/start-validator-client.sh
@@ -16,43 +16,14 @@ WALLET_PASSFILE=$DATADIR/secrets/$WALLET_NAME.pass
 
 if [ "$START_VALIDATOR" != "" ]; then
 	if [ "$IMPORT_LAUNCHPAD_KEYSTORES" != "" ]; then
-		echo $LAUNCHPAD_KEYSTORE_PASSWD | lighthouse \
+		lighthouse \
 			--network $NETWORK \
 			account validator import \
 			--directory /root/validator_keys \
 			--reuse-password \
 			--stdin-inputs
 	else
-		if [ ! -d $DATADIR ]; then
-			mkdir $DATADIR
-		fi
-
-		if [ ! -d $DATADIR/secrets ]; then
-			cd $DATADIR; mkdir secrets
-		fi
-
-		if [ ! -d $DATADIR/wallets ]; then
-			lighthouse \
-				--debug-level $DEBUG_LEVEL \
-				--network $NETWORK \
-				account \
-				wallet \
-				create \
-				--name $WALLET_NAME \
-				--password-file $WALLET_PASSFILE
-		else
-			echo "Wallet directory already exists. Will not create wallet."
-		fi
-
-		lighthouse \
-			--debug-level $DEBUG_LEVEL \
-			--network $NETWORK \
-			account \
-			validator \
-			create \
-			--wallet-name $WALLET_NAME \
-			--wallet-password $WALLET_PASSFILE \
-			--at-most $VALIDATOR_COUNT
+		echo "Wallet not imported"
 	fi
 
 	exec lighthouse \
